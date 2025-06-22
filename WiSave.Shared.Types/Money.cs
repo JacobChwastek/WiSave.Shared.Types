008132d5cccc8450805421399dbc306d26af241e
Qbash <jacobchwastek@gmail.com>
2025-06-23 00:25:04 +0200

    Extend Money.cs

--- WiSave.Shared.Types/Money.cs
+++ WiSave.Shared.Types/Money.cs
@@ -19,10 +19,9 @@ public record Money
     public decimal Amount { get; init; }
     public Currency Currency { get; init; }
 
-    public static Money operator *(Money money, uint times)
-    {
-        return money with { Amount = money.Amount * times };
-    }
+    public static Money Zero(Currency currency) => new(0m, currency);
+    
+    public static Money operator *(Money money, uint times) => money with { Amount = money.Amount * times };
 
     public static Money operator +(Money left, Money right)
     {
@@ -32,5 +31,36 @@ public record Money
         }
 
         return left with { Amount = left.Amount + right.Amount };
+    }
+    
+    
+    public static Money operator /(Money money, decimal divisor)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(divisor);
+
+        return new Money(money.Amount / divisor, money.Currency);
+    }
+
+    public static Money operator *(Money money, decimal multiplier)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(multiplier);
+
+        return new Money(money.Amount * multiplier, money.Currency);
+    }
+    
+    public static bool operator >(Money left, Money right)
+    {
+        if (left.Currency != right.Currency)
+            throw new MoneyOperationsArePossibleOnlyForSameCurrencyException();
+    
+        return left.Amount > right.Amount;
+    }
+
+    public static bool operator <(Money left, Money right)
+    {
+        if (left.Currency != right.Currency)
+            throw new MoneyOperationsArePossibleOnlyForSameCurrencyException();
+    
+        return left.Amount < right.Amount;
     }
 }
